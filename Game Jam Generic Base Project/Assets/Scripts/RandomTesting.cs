17137a8979ad49f7e41b0114580ef98bc3bcb312
Clauvin <clauvin_almeida@hotmail.com>
2019-01-31 20:46:28 -0200

    Added class for testing stuff quickly

---
+++ Game Jam Generic Base Project/Assets/Scripts/RandomTesting.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using SupportV4Jam;
+
+public class RandomTesting : MonoBehaviour {
+
+    public float value;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        DoStuff();
+	}
+
+    public void DoStuff()
+    {
+        value += Time.deltaTime;
+        SpinTheSkybox.UpdateSkyRotation(value);
+    }
+}
