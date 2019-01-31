eee50a6c6719c3da9b1076ac8816044fd941f314
Clauvin <clauvin_almeida@hotmail.com>
2019-01-31 21:20:52 -0200

    Added MainSceneData class

---
+++ Game Jam Generic Base Project/Assets/Libraries/SceneData.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace ScenesData {
+
+    public static class MainSceneData
+    {
+        public static bool is_paused = false;
+        public static bool is_alive = true;
+        public static bool your_turn = true;
+        public static bool enemy_turn = false;
+        public static float time = 0;
+    }
+
+}
