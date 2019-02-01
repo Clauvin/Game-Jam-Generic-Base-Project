2ea525a708d1fdab506f5d2d35513d8ffc792b43
Clauvin <clauvin_almeida@hotmail.com>
2019-02-01 00:57:59 -0200

    Added ProjectileData

---
+++ Game Jam Generic Base Project/Assets/Scripts/ProjectileData.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ProjectileData : MonoBehaviour {
+
+    public Vector3 velocity;
+    public Vector3 angle;
+    public int damage;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
