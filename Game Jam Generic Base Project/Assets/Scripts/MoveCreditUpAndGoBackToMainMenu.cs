5b9c83b81230d3b5792305aa52745bc95a62a9b8
Clauvin <clauvin_almeida@hotmail.com>
2019-02-01 00:43:23 -0200

    Update MoveCreditUpAndGoBackToMainMenu.cs

--- Game Jam Generic Base Project/Assets/Scripts/MoveCreditUpAndGoBackToMainMenu.cs
+++ Game Jam Generic Base Project/Assets/Scripts/MoveCreditUpAndGoBackToMainMenu.cs
@@ -4,6 +4,10 @@ using UnityEngine;
 
 public class MoveCreditUpAndGoBackToMainMenu : MonoBehaviour {
 
+    public float y_velocity_per_frame;
+    public float position_to_trigger_main_menu_loading;
+    private float gambiarra = 138;
+
 	// Use this for initialization
 	void Start () {
 		
@@ -11,6 +15,20 @@ public class MoveCreditUpAndGoBackToMainMenu : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-		
-	}
+        MoveUp();
+        TriggerToLoadMainMenu();
+    }
+
+    void MoveUp()
+    {
+        GetComponent<RectTransform>().Translate(new Vector3(0, y_velocity_per_frame, 0), Space.World);
+    }
+
+    void TriggerToLoadMainMenu()
+    {
+        if (GetComponent<RectTransform>().position.y >= position_to_trigger_main_menu_loading + gambiarra)
+        {
+            SupportV4Jam.LoadScene.LoadMainScreen();
+        }
+    }
 }
