84090587f022902bd8618032cbe90516330931ee
Clauvin <clauvin_almeida@hotmail.com>
2019-02-02 00:23:24 -0200

    Created DontDetroyOnLoad

---
+++ Game Jam Generic Base Project/Assets/Scripts/DontDestroyOnLoad.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DontDestroyOnLoad : MonoBehaviour {
+
+    public string tag_name;
+    public bool have_jumped = false;
+
+    void Awake()
+    {
+        SceneManager.activeSceneManager += OnSceneChanged;
+        
+        if (!have_jumped)
+        {
+            Gameobject[] this_objects = GameObject.FindGameObjectsWithTag(tag_name);
+
+            if (objs.Length > 1)
+            {
+                Destroy(this.gameobject);
+            }
+
+       }
+
+        DontDestroyOnLoad(this);
+    }
+
+    void OnSceneChanged()
+    {
+        this.have_jumped = true;
+    }
+}
