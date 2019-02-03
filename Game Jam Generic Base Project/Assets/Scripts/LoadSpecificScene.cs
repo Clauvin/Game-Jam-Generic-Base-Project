3a260bc3d628286d9951c9cb84e8c98891c6b017
Clauvin <clauvin_almeida@hotmail.com>
2019-02-02 22:32:49 -0200

    Added LoadSpecificScene

---
+++ Game Jam Generic Base Project/Assets/Scripts/LoadSpecificScene.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LoadSpecificScene : MonoBehaviour {
+
+    public AbstractConditionToLoadScene condition_to_load_scene;
+    public SupportV4Jam.Game_Scenes qual_cena;
+    public bool time_to_load_scene;
+
+    // Use this for initialization
+    void Start () {
+		if (condition_to_load_scene == null)
+        {
+            condition_to_load_scene = GetComponent<AbstractConditionToLoadScene>();
+        }
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        IsTimeToLoadScene();
+
+		if (time_to_load_scene)
+        {
+            LoadTheScene();
+        }
+	}
+
+    public void IsTimeToLoadScene()
+    {
+        time_to_load_scene = condition_to_load_scene.ConditionToLoadHasBeenFulfilled();
+    }
+
+    public void LoadTheScene()
+    {
+        SupportV4Jam.LoadScene.Load((int)qual_cena);
+    }
+}
