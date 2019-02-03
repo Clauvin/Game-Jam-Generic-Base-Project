4a1989782e221509a7a39501562c1862afd7b622
Clauvin <clauvin_almeida@hotmail.com>
2019-02-02 22:32:37 -0200

    Added TimeConditionToLoadScene

---
+++ Game Jam Generic Base Project/Assets/Scripts/TimeConditionToLoadScene.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TimeConditionToLoadScene : AbstractConditionToLoadScene {
+
+    public float actual_time;
+    public float time_to_load_scene;
+
+    public void Update()
+    {
+        CountTime();
+    }
+
+    public override bool ConditionToLoadHasBeenFulfilled()
+    {
+        if (actual_time >= time_to_load_scene) return true;
+        else return false;
+    }
+
+    public void CountTime()
+    {
+        actual_time += Time.deltaTime;
+    }
+}
